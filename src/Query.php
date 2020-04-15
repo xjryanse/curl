fdda77f1621ff4d92902df398691fda15b0de8de
xiaowq <35436546@qq.com>
2020-04-15 15:55:59 +0800

    1

--- src/Query.php
+++ src/Query.php
@@ -70,4 +70,23 @@ class Query
         curl_close($ch);
         return json_decode($output);
     }
+	/**
+	 * POST请求，兼容swoole websocket
+	 */
+    public function post( $url,$data)
+    {
+        $headerArray = [];
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,FALSE);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl,CURLOPT_HTTPHEADER,$headerArray);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($output,true);        
+    }	
 }
